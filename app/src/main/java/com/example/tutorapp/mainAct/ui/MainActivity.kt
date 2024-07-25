ccf86145faa71ba1d0e29a4252c852cc59924e5f
Vlad <vlad20062003@yandex.ru>
2024-07-25 16:40:53 +0300

    FragmentChanger.kt

--- app/src/main/java/com/example/tutorapp/mainAct/ui/MainActivity.kt
+++ app/src/main/java/com/example/tutorapp/mainAct/ui/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.tutorapp.mainAct.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tutorapp.R
-import com.example.tutorapp.account.ui.AccountMain
-import com.example.tutorapp.learning.ui.LearningMain
-import com.example.tutorapp.practice.ui.PracticeMain
+import com.example.tutorapp.mainAct.domain.FragmentChanger
 import com.example.tutorapp.theory.ui.TheoryMain
 
 class MainActivity : AppCompatActivity() {
@@ -37,36 +34,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ChangeFragment(view: View) {
-        when (view.id) {
-            R.id.goToTheory -> {
-                Log.d("click", "theory")
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragmentViewHolder, TheoryMain.newInstance())
-                    .commit()
-            }
-            R.id.goToAccount -> {
-                Log.d("click", "acc")
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragmentViewHolder, AccountMain.newInstance())
-                    .commit()
-            }
-            R.id.goToLearning -> {
-                Log.d("click", "learn")
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragmentViewHolder, LearningMain.newInstance())
-                    .commit()
-            }
-            R.id.goToPractice -> {
-                Log.d("click", "practice")
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragmentViewHolder, PracticeMain.newInstance())
-                    .commit()
-            }
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentViewHolder, FragmentChanger(view))
+            .commit()
     }
 
 }
